1d09c45628099cf109110ef9801bf1d8cb29236d
Cantlin Ashrowan <cantlin@ashrowan.com>
2011-11-11 12:44:22 +0000

    prompt for password immediatly

--- cap.py
+++ cap.py
@@ -16,13 +16,13 @@ parser.add_option("--uri", type="string", dest="base_uri", default="http://cantl
 parser.add_option("-p", "--password", type="string", dest="ftp_password", default=False)
 (options, args) = parser.parse_args()   
 
+if not options.ftp_password:
+    options.ftp_password = getpass.getpass("Password for \"" + options.ftp_user + "@" + options.ftp_host + "\": ")
+
 os.chdir(options.local_dir)
 file_name = options.file_name if options.file_name else "ss_" + str(int(time.time()))
 file_name += '.' +  options.file_format
 subprocess.call(['screencapture', '-m', '-T3', '-t' + options.file_format, file_name])
-    
-if not options.ftp_password:
-    options.ftp_password = getpass.getpass("Password for \"" + options.ftp_user + "@" + options.ftp_host + "\": ")
     
 ftp = FTP(options.ftp_host)
 ftp.login(options.ftp_user, options.ftp_password)
